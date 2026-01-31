dcba39c3a703178ac912d0a161fd45c51b39d58e
MYKOLA YAKYMCHUK <m.yakymchuk5@gmail.com>
2026-01-31 22:28:31 +0100

    Thread pool

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,19 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private static final Random RANDOM = new Random();
+
     @Override
     public String call() throws InterruptedException {
         // write your code here
-        return null;
+        int duration = (RANDOM.nextInt(5) + 1) * 100; // 100–500 ms
+        Thread.sleep(duration);
+
+        return "Task duration was " + duration
+                + " ms, execution finished at " + LocalTime.now();
     }
 }
